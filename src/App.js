7227877372dcd6542a7f76ce823c7f5980a474f8
Emilio Barrea <barrera_emilio@yahoo.es>
2021-03-12 22:02:12 +0000

    Elimar Tareas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import React, { useState } from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import shortid from 'shortid';
 
 
@@ -26,6 +26,17 @@ function App() {
 		setTask("");
 	}
 
+	const deleteTask = (id) => {
+		// console.log("hi form delete Task....");
+		const filteredTasks = tasks.filter(t => t.id !== id);
+		setTasks(filteredTasks);
+		//  console.log(tasks);
+	};
+
+	const h5Style = {
+		color: "red"
+	};
+
 	return (
 		<div className="container mt-5">
 			<h1>Tareas</h1>
@@ -33,17 +44,30 @@ function App() {
 			<div className="row">
 				<div className="col-8">
 					<h4 className="text-center">Lista de Tareas</h4>
-					<ul className="list-group">
-						{
-							tasks.map((task) => (
-								<li className="list-group-item" key={task.id}>
-								<span className="lead">{ task.name }</span>
-								<button className="btn btn-danger btn-sm float-right">Eliminar</button>
-								<button className="btn btn-warning btn-sm float-right mx-2">Editar</button>
-							</li>
-							))						
-						};
-					</ul>
+					{
+						size(tasks) > 0 ? (
+							<ul className="list-group">
+								{
+									tasks.map((task) => (
+										<li className="list-group-item" key={task.id}>
+											<span className="lead">{task.name}</span>
+											<button
+												className="btn btn-danger btn-sm float-right"
+												onClick={() => deleteTask(task.id)}>
+												Eliminar
+									</button>
+											<button
+												className="btn btn-warning btn-sm float-right mx-2">
+												Editar
+										</button>
+										</li>
+									))
+								}
+							</ul>
+						) : (
+							<h5 className="text-center" style={h5Style}>No hay  Tareas Programadas!</h5>
+						)
+					}
 				</div>
 				<div className="col-4">
 					<h4 className="text-center">Formulario</h4>
